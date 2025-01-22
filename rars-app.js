f34bc6c7f66f9b332e41b1a06e9231a5d1c904ae
RARS-Instruments-Company <muhammadrushaanabbas@gmail.com>
2025-01-22 23:27:53 +0400

    frontend

---
+++ rars-app.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        name: 'RARS',
+        type: 'Instruments Co.',
+        address: 'Flat 9C, Building 5, J-Sector, Malir Cantt. Karachi, Pakistan',
+        logo_colored: 'assets/Rars-Logo-HD-Color.jpg',
+    }
+});
